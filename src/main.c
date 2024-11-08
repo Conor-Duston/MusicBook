a0e7a30d708e611d218c38da99c490ce3845643a
cdthereader <conor.duston@gmail.com>
2024-11-08 15:59:05 -0500

    Updated main to retrieve and sort files

--- src/main.c
+++ src/main.c
@@ -18,6 +18,9 @@
 
 #define MOUNT_POINT "/sdc"
 
+#define MAX_NUM_PAGES 7
+#define MAX_FILE_NAME_LENGTH 13
+
 void app_main() {
 
     char *ourTaskName = pcTaskGetName(NULL);
@@ -28,9 +31,10 @@ void app_main() {
     ESP_LOGI(ourTaskName, "Initializing SD card");
 
     esp_vfs_fat_mount_config_t mount_config = {
-        .format_if_mount_failed = false,
+        .format_if_mount_failed = true,
         .disk_status_check_enable = true,
-        .max_files = 2
+        .max_files = 2,
+        .allocation_unit_size = 4096
     };
 
     sdmmc_card_t *card;
@@ -78,6 +82,58 @@ void app_main() {
     ESP_LOGI(ourTaskName, "FS mounted");
 
     sdmmc_card_print_info(stdout, card);
+
+    ESP_LOGI(ourTaskName, "Opening Base Directory and Searching for files");
+    
+    FF_DIR baseDir;
+    FILINFO file_info;
+
+    FRESULT f_res;
+
+    TCHAR f_names[MAX_NUM_PAGES][MAX_FILE_NAME_LENGTH];
+
+    f_res = f_opendir(&baseDir , "");
+    
+    if (f_res != F_OK) {
+        ESP_LOGE(ourTaskName, "Could not open directory (%s)\n", mount_point);
+        ESP_LOGE(ourTaskName, "FATFS Error: %d", f_res);
+        return;
+    }
+
+    int file = 0;
+
+    for (;;) {
+        f_res = f_readdir(&baseDir, &file_info);
+        if (f_res != FR_OK || file_info.fname[0] == 0) break;
+
+        if (file_info.fattrib & AM_DIR) {
+            ESP_LOGI(ourTaskName, "Folder: %s", file_info.fname);
+        } else {
+
+            ESP_LOGI(ourTaskName, "File Name: %s", file_info.fname);
+            ESP_LOGI(ourTaskName, "\tSize: %10lu", file_info.fsize);
+            if (file < MAX_NUM_PAGES) {
+                strncpy(f_names[file], file_info.fname, MAX_FILE_NAME_LENGTH);
+                file ++;
+            }
+        }
+    }
+    
+    
+    f_res = f_closedir(&baseDir);
+
+    if (f_res != F_OK) {
+        ESP_LOGE(ourTaskName, "FATFS Error when opening a file: %d", f_res);
+        return;
+    }
+    
+    // Sort files by alphanumeric values, IE 0 - 9 a - z, ignoring case, for later retrieval
+    qsort(f_names, file, MAX_FILE_NAME_LENGTH, strcasecmp);
+
+    ESP_LOGI(ourTaskName, "File Order: \n");
+    for (int i = 0; i <file; ++i) {
+        ESP_LOGI(ourTaskName, "%s\n", f_names[i]);
+    }
 
     while (1){
         vTaskDelay (10000);
